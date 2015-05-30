eb30563cc99a655b5846755c6299bdb989194b98
FawkesThePhoney <swenzel@wustl.edu>
2015-05-30 10:38:50 -0500

    Unity files

---
+++ LowPoly/Assets/DungeonCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonCamera : MonoBehaviour {
+	public GameObject target;
+	Vector3 offset;
+	public float damping = 1;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 desiredPosition = target.transform.position + offset;
+		Vector3 position = Vector3.Lerp (transform.position, desiredPosition, Time.deltaTime * damping);
+		transform.position = position;
+
+		transform.LookAt (target.transform.position);
+	}
+}
---
+++ LowPoly/Assets/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+	public GameObject target;
+	Vector3 offset;
+	public float damping = 1;
+
+	// Use this for initialization
+	void Start () {
+		offset = target.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float currentAngle = transform.eulerAngles.y;
+		float desiredAngle = target.transform.eulerAngles.y;
+		float angle = Mathf.LerpAngle (currentAngle, desiredAngle, Time.deltaTime * damping);
+		Quaternion rotation = Quaternion.Euler (0, angle, 0);
+
+		transform.position = target.transform.position - (rotation * offset);
+		transform.LookAt (target.transform);
+	}
+}
